b8fcab9d0091749d27954d9e9b0b913bf62d4689
mdashmiller <m.robert.miller@gmail.com>
2019-08-28 13:44:01 -0500

    add apollo-server

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const express = require('express')
-const graphqlHTTP = require('express-graphql')
-const { buildSchema } = require('graphql')
+const { ApolloServer, gql } = require('apollo-server')
 const crypto = require('crypto')
 
 const db = {
@@ -41,7 +39,7 @@ const db = {
   ]
 }
 
-const schema = buildSchema(`
+const typeDefs = gql`
   type Query {
     users: [User!]!
     user(id: ID!): User 
@@ -66,33 +64,32 @@ const schema = buildSchema(`
     authorId: ID!
     createdAt: String!
   }
-`)
+`
 
-const rootValue = {
-  users: () => db.users,
-  user: ({ id }) => {
-    return db.users.find(user => id === user.id)
+const resolvers = {
+  Query: {
+    users: () => db.users,
+    user: (parent, { id }) => db.users.find(user => id === user.id),
+    posts: () => db.posts
   },
-  posts: () => db.posts,
-  addUser: ({ name, email }) => {
-    const user = {
-      id: crypto.randomBytes(10).toString('hex'),
-      name,
-      email
-    }
-    
-    db.users.push(user)
+  Mutation: {
+    addUser: (parent, { name, email }) => {
+      const user = {
+        id: crypto.randomBytes(10).toString('hex'),
+        name,
+        email
+      }
 
-    return user
+      db.users.push(user)
+
+      return user
+    }
+  },
+  User: {
+    posts: user => db.posts.filter(post => post.authorId === user.id)
   }
 }
 
-const app = express()
+const server = new ApolloServer({ typeDefs, resolvers })
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  rootValue,
-  graphiql: true
-}))
-
-app.listen(5000, () => console.log('Listening on 5000'))
+server.listen().then(({ url }) => console.log(url))
