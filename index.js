86ec64193d9c3e1f41180820e9da8aa1a994eadb
mdashmiller <m.robert.miller@gmail.com>
2019-08-28 12:42:07 -0500

    add express

--- index.js
+++ index.js
@@ -1,41 +1,98 @@
-const { graphql, buildSchema } = require('graphql')
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
+const crypto = require('crypto')
 
 const db = {
   users: [
     { id: '1', name: 'marty', email: 'mcfly@gmail.com' },
     { id: '2', name: 'jen', email: 'thegirlfriend@gmail.com' },
     { id: '3', name: 'doc', email: 'dbrown@gmail.com' },
+  ],
+  posts: [
+    { 
+      id: crypto.randomBytes(10).toString('hex'),
+      title: 'title 1',
+      content: 'content for post 1',
+      authorId: '1',
+      createdAt: Date.now()
+    },
+    { 
+      id: crypto.randomBytes(10).toString('hex'),
+      title: 'title 2',
+      content: 'content for post 2',
+      authorId: '2',
+      createdAt: Date.now()
+    },
+    { 
+      id: crypto.randomBytes(10).toString('hex'),
+      title: 'title 3',
+      content: 'content for post 3',
+      authorId: '3',
+      createdAt: Date.now()
+    },
+    { 
+      id: crypto.randomBytes(10).toString('hex'),
+      title: 'title 4',
+      content: 'content for post 4',
+      authorId: '1',
+      createdAt: Date.now()
+    },
   ]
 }
 
 const schema = buildSchema(`
   type Query {
     users: [User!]!
+    user(id: ID!): User 
+    posts: [Post!]!
+  }
+
+  type Mutation {
+    addUser(name: String!, email: String!): User!
   }
 
   type User {
     id: ID!
     name: String!
     email: String!
+    posts: [Post!]!
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    authorId: ID!
+    createdAt: String!
   }
 `)
 
 const rootValue = {
-  users: () => db.users
+  users: () => db.users,
+  user: ({ id }) => {
+    return db.users.find(user => id === user.id)
+  },
+  posts: () => db.posts,
+  addUser: ({ name, email }) => {
+    const user = {
+      id: crypto.randomBytes(10).toString('hex'),
+      name,
+      email
+    }
+    
+    db.users.push(user)
+
+    return user
+  }
 }
 
-graphql(
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
   schema,
-  `
-    {
-      users {
-        email
-      }
-    }
-  `,
-  rootValue
-).then(res => 
-  console.dir(res, { depth: null })
-).catch(
-  console.error
-)
+  rootValue,
+  graphiql: true
+}))
+
+app.listen(5000, () => console.log('Listening on 5000'))
